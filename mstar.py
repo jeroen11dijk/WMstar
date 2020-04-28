f1f78bfdecfd6d0456f2e3bde031eb558b2e4c22
Viliam Vadocz <viliam.vadocz@gmail.com>
2020-04-28 17:03:38 +0200

    Change lists to dataclass to make code readable

--- mstar.py
+++ mstar.py
@@ -3,49 +3,49 @@ import heapq
 import itertools
 import time
 from dataclasses import dataclass
+from typing import Dict, Tuple, List, Set, Optional
 
 import networkx as nx
 from matplotlib.pyplot import show
 
 
 @dataclass
-class ConfigurationInfo:
+class ConfigInfo:
     cost: float
-    collision_set: set
-    back_set: []
-    back_ptr: ()
+    collision_set: Set[str]
+    back_set: List[str]
+    back_ptr: Optional[Tuple[str]]
 
 
 def Mstar(graph, v_I, v_F):
     # Dictionary for every configuration
-    # List = [cost, collision set, back_set, back_ptr]
-    configurations = {}
+    configurations: Dict[Tuple[str], ConfigInfo] = {}
     policy = []
     for target in v_F:
         policy.append(nx.dijkstra_predecessor_and_distance(G, target))
-    configurations[v_I] = [0, set(), [], None]
+    configurations[v_I] = ConfigInfo(0, set(), [], None)
     open = []
-    heapq.heappush(open, (configurations[v_I][0] + heuristic_configuration(v_I, policy), v_I))
+    heapq.heappush(open, (configurations[v_I].cost + heuristic_configuration(v_I, policy), v_I))
     while len(open) > 0:
         v_k = heapq.heappop(open)[1]
         if v_k == v_F:
             res = [v_F]
-            while configurations[v_k][3] is not None:
-                res.append(configurations[v_k][3])
-                v_k = configurations[v_k][3]
-            return res[::-1], configurations[v_F][0]
+            while configurations[v_k].back_ptr is not None:
+                res.append(configurations[v_k].back_ptr)
+                v_k = configurations[v_k].back_ptr
+            return res[::-1], configurations[v_F].cost
         if len(phi(v_k)) == 0:
             V_k = get_limited_neighbours(v_k, configurations, graph, policy)
             for v_l in V_k:
                 collisions = phi(v_l)
-                configurations[v_l][1].update(collisions)
-                configurations[v_l][2].append(v_k)
-                backprop(v_k, configurations[v_l][1], open, configurations, policy)
+                configurations[v_l].collision_set.update(collisions)
+                configurations[v_l].back_set.append(v_k)
+                backprop(v_k, configurations[v_l].collision_set, open, configurations, policy)
                 f = get_edge_weight(v_k, v_l, graph)
-                if len(collisions) == 0 and configurations[v_k][0] + f < configurations[v_l][0]:
-                    configurations[v_l][0] = configurations[v_k][0] + f
-                    configurations[v_l][3] = v_k
-                    heapq.heappush(open, (configurations[v_l][0] + heuristic_configuration(v_l, policy), v_l))
+                if len(collisions) == 0 and configurations[v_k].cost + f < configurations[v_l].cost:
+                    configurations[v_l].cost = configurations[v_k].cost + f
+                    configurations[v_l].back_ptr = v_k
+                    heapq.heappush(open, (configurations[v_l].cost + heuristic_configuration(v_l, policy), v_l))
     return "No path exists, or I am a retard"
 
 
@@ -55,7 +55,7 @@ def get_limited_neighbours(v_k, configurations, graph, policy):
     for i in range(len(v_k)):
         vi_k = v_k[i]
         options_i = []
-        if i in configurations[v_k][1]:
+        if i in configurations[v_k].collision_set:
             # ADD all the neighbours
             options_i.append(vi_k)
             for nbr in graph[vi_k]:
@@ -72,24 +72,24 @@ def get_limited_neighbours(v_k, configurations, graph, policy):
     if len(options) == 1:
         option = options[0][0]
         if option not in configurations:
-            configurations[option] = [float('inf'), set(), [], None]
+            configurations[option] = ConfigInfo(float('inf'), set(), [], None)
         V_k.append(options[0][0])
         return V_k
     for element in itertools.product(*options):
         if element not in configurations:
-            configurations[element] = [float('inf'), set(), [], None]
+            configurations[element] = ConfigInfo(float('inf'), set(), [], None)
         V_k.append(element)
     return V_k
 
 
 def backprop(v_k, C_l, open, configurations, policy):
-    C_k = configurations[v_k][1]
+    C_k = configurations[v_k].collision_set
     if not C_l.issubset(C_k):
         C_k.update(C_l)
         if v_k not in [k for v, k in open]:
-            heapq.heappush(open, (configurations[v_k][0] + heuristic_configuration(v_k, policy), v_k))
-        for v_m in configurations[v_k][2]:
-            backprop(v_m, configurations[v_k][1], open, configurations, policy)
+            heapq.heappush(open, (configurations[v_k].cost + heuristic_configuration(v_k, policy), v_k))
+        for v_m in configurations[v_k].back_set:
+            backprop(v_m, configurations[v_k].collision_set, open, configurations, policy)
 
 
 def get_edge_weight(v_k, v_l, graph):
@@ -101,7 +101,7 @@ def get_edge_weight(v_k, v_l, graph):
 def phi(v_k):
     seen = set()
     double = list()
-    for i, val in enumerate(v_k):
+    for val in v_k:
         if val in seen:
             double.append(val)
         else:
