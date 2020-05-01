1d34365bb68fe5fb3e156f13a7ac125a64931280
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-05-01 11:41:24 +0200

    Even more nice code

--- benchmark.py
+++ benchmark.py
@@ -15,16 +15,13 @@ def solve(problem):
     configs = Mstar(graph, v_I, v_F)[0]
     res = []
     for i in range(len(problem.starts)):
-        path = []
-        for t_config in configs:
-            path.append(list(t_config[i]))
-        res.append(path)
+        res.append([list(config[i]) for config in configs])
     return res
 
 
-benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 6, "M*", "Version 1.01 (Without inversing)", False)
+benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 6, "M*", "Version 1.01 (Without inversing)", True)
 
 for problem in benchmarker:
     problem.add_solution(solve(problem))
 
-benchmarker.submit()
+# benchmarker.submit()
