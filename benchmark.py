66563ef976c4b584a31bf48b9dd94f40b12fac38
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-05-01 11:43:21 +0200

    Added some comments

--- benchmark.py
+++ benchmark.py
@@ -5,14 +5,18 @@ from mstar import Mstar
 
 
 def solve(problem):
+    # Create a networkx graph
     graph = grid_graph([len(problem.grid), len(problem.grid[0])])
     for i in range(len(problem.grid)):
         for j in range(len(problem.grid[0])):
             if problem.grid[i][j] == 1:
                 graph.remove_node((j, i))
+    # Create V_I and v_F
     v_I = tuple(tuple(start) for start in problem.starts)
     v_F = tuple(tuple(target) for target in problem.goals)
+    # Run the algorithm
     configs = Mstar(graph, v_I, v_F)[0]
+    # Convert to the accepted format
     res = []
     for i in range(len(problem.starts)):
         res.append([list(config[i]) for config in configs])
