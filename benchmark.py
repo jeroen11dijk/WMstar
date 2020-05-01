aabd28fd38c504d4ef4212de2a5d6d5c145ca990
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-05-01 10:21:38 +0200

    The code now works for the benchmarks

--- benchmark.py
+++ benchmark.py
@@ -8,7 +8,7 @@ from mstar import Mstar
 # Problem 1 has a grid of 28x26 yet the width and height are given as 27x26
 
 def solve(problem):
-    graph = grid_graph([28, problem.height])
+    graph = grid_graph([len(problem.grid[0]), len(problem.grid)])
     for i in range(len(problem.grid)):
         for j in range(len(problem.grid[0])):
             if problem.grid[i][j] == 1:
@@ -26,19 +26,14 @@ def solve(problem):
     for i in range(len(problem.starts)):
         path = []
         for t_config in configs:
-            path.append(list(t_config[i]))
+            path.append([t_config[i][1], t_config[i][0]])
         res.append(path)
-    """
-    Now paths looks like:
-
-    paths = [path agent 1, path agent 2, ..]
-    path agent 1 = [pos agent 1 at time 0, pos agent 1 at time 1, .., pos agent 1 at finishing time]
-    pos = [x coordinate, y coordinate]
-    """
-
     return res
 
 
-benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 1, "M*", "Version 1", False)
+benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 6, "M*", "Version 1", False)
+
 for problem in benchmarker:
-    solve(problem)
+    problem.add_solution(solve(problem))
+
+# benchmarker.submit()
