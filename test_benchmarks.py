7a487e3b2492e0b755714a172568b236400fa350
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-05-07 11:33:11 +0200

    Remove the runtime check

--- test_benchmarks.py
+++ test_benchmarks.py
@@ -1,17 +1,15 @@
 import math
 import time
 
-import networkx as nx
 from mapfw import MapfwBenchmarker
 from networkx import grid_graph
 
 from mstar import Mstar
 
 
-def main(G, v_I, v_W, v_F, time_budget, min_cost):
+def main(G, v_I, v_W, v_F, min_cost):
     start = time.time()
     res = Mstar(G, v_I, v_W, v_F)
-    assert time.time() - start < time_budget
     assert res[1] < min_cost or math.isclose(res[1], min_cost)
     paths = res[0]
     for i in range(len(v_I)):
@@ -24,11 +22,11 @@ def main(G, v_I, v_W, v_F, time_budget, min_cost):
         assert v_F[i] == paths[i][-1] or list(v_F[i]) == paths[i][-1]
 
 
-def benchmark(i, time_budget, min_cost):
+def benchmark(i, min_cost):
     benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", i, "M*", "Test", True)
     for problem in benchmarker:
         graph, v_I, v_W, v_F = setup_benchmark(problem)
-        main(graph, v_I, v_W, v_F, time_budget, min_cost)
+        main(graph, v_I, v_W, v_F, min_cost)
 
 
 def setup_benchmark(problem):
@@ -51,19 +49,19 @@ def setup_benchmark(problem):
 
 class TestBenchmarks:
     def test_benchmark_1(self):
-        benchmark(1, 0.02, 50)
+        benchmark(1, 50)
 
     def test_benchmark_2(self):
-        benchmark(2, 0.02, 38)
+        benchmark(2, 38)
 
     def test_benchmark_3(self):
-        benchmark(3, 0.02, 76)
+        benchmark(3, 76)
 
     def test_benchmark_4(self):
-        benchmark(4, 0.02, 17)
+        benchmark(4, 17)
 
     def test_benchmark_6(self):
-        benchmark(6, 0.02, 21)
+        benchmark(6, 21)
 
     def test_benchmark_17(self):
-        benchmark(17, 9, 41)
+        benchmark(17, 41)
