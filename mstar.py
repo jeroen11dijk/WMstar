36cc406677b0047c623f66c8acafc6159b0120a4
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-05-09 12:03:38 +0200

    Changed configurations so that keys are (configuration, target_indices)

--- mstar.py
+++ mstar.py
@@ -8,19 +8,18 @@ import networkx as nx
 
 
 class Config:
-    def __init__(self, target_indices):
+    def __init__(self):
         self.cost = float('inf')
         self.heuristic = float('inf')
         self.collisions = set()
         self.back_set = []
-        self.target_indices = target_indices
         self.back_ptr = []
 
     def __str__(self):
-        return str([self.cost, self.collisions, self.back_set, self.target_indices, self.back_ptr])
+        return str([self.cost, self.collisions, self.back_set, self.back_ptr])
 
     def __repr__(self):
-        return str([self.cost, self.collisions, self.back_set, self.target_indices, self.back_ptr])
+        return str([self.cost, self.collisions, self.back_set, self.back_ptr])
 
 
 class Mstar:
@@ -31,7 +30,6 @@ class Mstar:
         self.v_F = v_F
         self.n_agents = len(v_I)
         self.configurations = {}
-        self.phi_dictionary = {}
         self.policies = []
         self.distances = []
         self.targets = []
@@ -53,64 +51,60 @@ class Mstar:
             self.distances.append(distance_i)
             self.targets.append(targets_i)
         self.open = []
-        self.configurations[v_I] = Config([0] * self.n_agents)
-        self.configurations[v_I].cost = 0
-        heapq.heappush(self.open, (self.heuristic_configuration(v_I, tuple([0] * self.n_agents)), v_I))
+        self.configurations[(v_I, (0,) * self.n_agents)] = Config()
+        self.configurations[(v_I, (0,) * self.n_agents)].cost = 0
+        heapq.heappush(self.open, (self.heuristic_configuration(v_I, tuple([0] * self.n_agents)), (v_I, (0,) * self.n_agents)))
 
     def solve(self):
         configurations = self.configurations
         while len(self.open) > 0:
-            v_k = heapq.heappop(self.open)[1]
-            if v_k == self.v_F and all(
-                    configurations[self.v_F].target_indices[i] + 1 == len(self.targets[i]) for i in
-                    range(len(self.v_F))):
-                configurations[self.v_F].back_ptr.append(self.v_F)
+            v_k, target_indices = heapq.heappop(self.open)[1]
+            if v_k == self.v_F and all(target_indices[i] + 1 == len(self.targets[i]) for i in range(len(self.v_F))):
+                configurations[(v_k, target_indices)].back_ptr.append(self.v_F)
                 res = []
                 for i in range(self.n_agents):
-                    res.append([list(config[i]) for config in configurations[self.v_F].back_ptr])
-                return res, configurations[self.v_F].cost
+                    res.append([list(config[i]) for config in configurations[(v_k, target_indices)].back_ptr])
+                return res, configurations[(v_k, target_indices)].cost
             if len(self.phi(v_k)) == 0:
-                V_k = self.get_limited_neighbours(v_k)
+                V_k = self.get_limited_neighbours(v_k, target_indices)
                 for v_l in V_k:
-                    v_l_collisions = self.phi(v_l)
-                    configurations[v_l].collisions.update(v_l_collisions)
-                    configurations[v_l].back_set.append(v_k)
-                    self.backprop(v_k, configurations[v_l].collisions)
-                    temp_target_indices = copy(configurations[v_k].target_indices)
+                    v_l_target_indices = list(target_indices)
                     for i in range(self.n_agents):
-                        if v_l[i] == self.targets[i][temp_target_indices[i]] and v_l[i] != self.v_F[i]:
-                            temp_target_indices[i] += 1
-                    v_l_target_indices = tuple(configurations[v_l].target_indices)
-                    f = self.get_edge_weight(v_k, v_l, tuple(temp_target_indices))
-                    new_cost_v_l = configurations[v_k].cost + f + self.heuristic_configuration(v_l, tuple(
-                        temp_target_indices))
-                    old_cost_v_l = configurations[v_l].cost + self.heuristic_configuration(v_l, v_l_target_indices)
-                    more_targets = math.isclose(new_cost_v_l, old_cost_v_l) and sum(
-                        configurations[v_l].target_indices) < sum(
-                        temp_target_indices)
-                    if len(v_l_collisions) == 0 and (new_cost_v_l < old_cost_v_l or more_targets):
-                        configurations[v_l].cost = configurations[v_k].cost + f
-                        configurations[v_l].back_ptr = configurations[v_k].back_ptr + [v_k]
-                        configurations[v_l].target_indices = temp_target_indices
-                        heuristic = self.heuristic_configuration(v_l, tuple(temp_target_indices))
-                        heapq.heappush(self.open, (configurations[v_l].cost + heuristic, v_l))
+                        if v_l[i] == self.targets[i][v_l_target_indices[i]] and v_l[i] != self.v_F[i]:
+                            v_l_target_indices[i] += 1
+                    v_l_target_indices = tuple(v_l_target_indices)
+                    v_l_collisions = self.phi(v_l)
+                    if (v_l, v_l_target_indices) not in configurations:
+                        configurations[(v_l, v_l_target_indices)] = Config()
+                    configurations[(v_l, v_l_target_indices)].collisions.update(v_l_collisions)
+                    configurations[(v_l, v_l_target_indices)].back_set.append((v_k, target_indices))
+                    self.backprop(v_k, target_indices, configurations[(v_l, v_l_target_indices)].collisions)
+
+                    f = self.get_edge_weight(v_k, v_l, v_l_target_indices)
+                    new_cost_v_l = configurations[(v_k, target_indices)].cost + f
+                    old_cost_v_l = configurations[(v_l, v_l_target_indices)].cost
+                    if len(v_l_collisions) == 0 and new_cost_v_l < old_cost_v_l:
+                        configurations[(v_l, v_l_target_indices)].cost = configurations[(v_k, target_indices)].cost + f
+                        configurations[(v_l, v_l_target_indices)].back_ptr = configurations[(v_k, target_indices)].back_ptr + [v_k]
+                        heuristic = self.heuristic_configuration(v_l, v_l_target_indices)
+                        heapq.heappush(self.open, (configurations[(v_l, v_l_target_indices)].cost + heuristic, (v_l, v_l_target_indices)))
         return "No path exists, or I am an idiot"
 
-    def get_limited_neighbours(self, v_k):
+    def get_limited_neighbours(self, v_k, target_indices):
         V_k = []
         options = []
         configurations = self.configurations
         for i in range(self.n_agents):
             vi_k = v_k[i]
             options_i = []
-            if i in configurations[v_k].collisions:
+            if i in configurations[(v_k, target_indices)].collisions:
                 # ADD all the neighbours
                 options_i.append(vi_k)
                 for nbr in self.graph[vi_k]:
                     options_i.append(nbr)
             else:
                 source = v_k[i]
-                target_index = configurations[v_k].target_indices[i]
+                target_index = target_indices[i]
                 policy = self.policies[i][target_index]
                 successors = policy[v_k[i]]
                 if len(successors) == 0:
@@ -120,29 +114,23 @@ class Mstar:
                         options_i.append(successor)
             options.append(options_i)
         if len(options) == 1:
-            option = options[0][0]
-            if option not in configurations:
-                configurations[option] = Config([0] * self.n_agents)
             V_k.append(options[0][0])
             return V_k
         for element in itertools.product(*options):
-            if element not in configurations:
-                configurations[element] = Config([0] * self.n_agents)
             V_k.append(element)
         return V_k
 
-    def backprop(self, v_k, C_l):
-        C_k = self.configurations[v_k].collisions
+    def backprop(self, v_k, target_indices, C_l):
+        C_k = self.configurations[(v_k, target_indices)].collisions
         if not C_l.issubset(C_k):
             C_k.update(C_l)
             # Technically we should check whether its not already in open
             # But that takes too much time and it will settle it self
             # if not any(v_k in configuration for configuration in open):
-            target_indices = tuple(self.configurations[v_k].target_indices)
             heapq.heappush(self.open,
-                           (self.configurations[v_k].cost + self.heuristic_configuration(v_k, target_indices), v_k))
-            for v_m in self.configurations[v_k].back_set:
-                self.backprop(v_m, self.configurations[v_k].collisions)
+                           (self.configurations[(v_k, target_indices)].cost + self.heuristic_configuration(v_k, target_indices), (v_k, target_indices)))
+            for v_m, v_m_target_indices in self.configurations[(v_k, target_indices)].back_set:
+                self.backprop(v_m, v_m_target_indices, self.configurations[(v_k, target_indices)].collisions)
 
     @lru_cache(maxsize=None)
     def get_edge_weight(self, v_k, v_l, target_indices):
