a27246b8e657b894d6493b95606453675d78d5b3
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-05-12 11:51:10 +0200

    Gitignore should now work

--- testing.py
+++ testing.py
@@ -1,15 +1,8 @@
 from mapfw import MapfwBenchmarker
-import Mstar_cpp
 from mstar import Mstar
 from test_benchmarks import setup_benchmark
 
-benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 17, "M*", "Simple Heuristic", True)
+benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 7, "M*", "Simple Heuristic", True)
 for problem in benchmarker:
     graph, v_I, v_W, v_F = setup_benchmark(problem)
     problem.add_solution(Mstar(graph, v_I, v_W, v_F).solve()[0])
-
-
-dictionary = {"A": 0, "B": 0, "C": 0}
-
-print(Mstar_cpp.NietJeroen(dictionary, "A"))
-print(Mstar_cpp.NietJeroen(dictionary, "HELLO"))
