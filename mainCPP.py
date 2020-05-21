8ce7c58a3fe75cea4d8ce85ea61762bca43aeec8
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-05-21 17:49:29 +0200

    PLS SEND HELP

--- mainCPP.py
+++ mainCPP.py
@@ -3,10 +3,11 @@ from copy import copy
 from Mstar_cpp import Mstar_cpp
 from mapfw import MapfwBenchmarker, get_all_benchmarks
 
-benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 19, "M*", "Cpp optimized but bad heuristic", False)
+benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 17, "M*", "Cpp optimized but bad heuristic", True)
 for problem in benchmarker:
     cpp_waypoints = copy(problem.waypoints)
     for i in range(len(cpp_waypoints)):
         if len(cpp_waypoints[i]) == 0:
             cpp_waypoints[i] = [[-1, -1]]
-    problem.add_solution(Mstar_cpp(problem.grid, problem.starts, cpp_waypoints, problem.goals).solve()[0])
+    # problem.add_solution(Mstar_cpp(problem.grid, problem.starts, cpp_waypoints, problem.goals).solve()[0])
+    print(Mstar_cpp(problem.grid, problem.starts, cpp_waypoints, problem.goals).solve()[0])
