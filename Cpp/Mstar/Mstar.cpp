85750c170ede31314f2fd1625d705069ec0c0731
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-05-22 17:30:10 +0200

    Updated the CPP a bit

--- Cpp/Mstar/Mstar.cpp
+++ Cpp/Mstar/Mstar.cpp
@@ -3,21 +3,20 @@
 #include "inc/Graph.h"
 #include "inc/Utils.h"
 
-using namespace std;
-
-Mstar::Mstar(vector<vector<int>> &grid, vector<pair<int, int>> &v_I_a, vector<vector<pair<int, int>>> &v_W_a,
-             vector<pair<int, int>> &v_F_a) {
+Mstar::Mstar(std::vector<std::vector<int>> &grid, std::vector<std::pair<int, int>> &v_I_a,
+             std::vector<std::vector<std::pair<int, int>>> &v_W_a,
+             std::vector<std::pair<int, int>> &v_F_a) {
     n_agents = v_I_a.size();
     graph = create_graph(grid);
-    for (pair<int, int> &start : v_I_a) {
+    for (std::pair<int, int> &start : v_I_a) {
         v_I.emplace_back(start.first, start.second);
     }
-    for (pair<int, int> &goal : v_F_a) {
+    for (std::pair<int, int> &goal : v_F_a) {
         v_F.emplace_back(goal.first, goal.second);
     }
-    for (const vector<pair<int, int>> &waypoints : v_W_a) {
-        vector<Coordinate> waypoints_i;
-        for (pair<int, int> waypoint : waypoints) {
+    for (const std::vector<std::pair<int, int>> &waypoints : v_W_a) {
+        std::vector<Coordinate> waypoints_i;
+        for (std::pair<int, int> waypoint : waypoints) {
             if (waypoint.first != -1 && waypoint.second != -1) {
                 waypoints_i.emplace_back(waypoint.first, waypoint.second);
             }
@@ -37,19 +36,19 @@ Mstar::Mstar(vector<vector<int>> &grid, vector<pair<int, int>> &v_I_a, vector<ve
         }
     }
     for (int i = 0; i != n_agents; i++) {
-        vector<unordered_map<Coordinate, vector<Coordinate>, coordinate_hash>> policy_i;
-        vector<unordered_map<Coordinate, int, coordinate_hash>> distance_i;
-        vector<Coordinate> targets_i;
+        std::vector<std::unordered_map<Coordinate, std::vector<Coordinate>, coordinate_hash>> policy_i;
+        std::vector<std::unordered_map<Coordinate, int, coordinate_hash>> distance_i;
+        std::vector<Coordinate> targets_i;
         for (Coordinate const &waypoint : v_W[i]) {
             if (graph.count(waypoint)) {
-                pair<unordered_map<Coordinate, vector<Coordinate>, coordinate_hash>, unordered_map<Coordinate, int, coordinate_hash>> dijkstra = dijkstra_predecessor_and_distance(
+                std::pair<std::unordered_map<Coordinate, std::vector<Coordinate>, coordinate_hash>, std::unordered_map<Coordinate, int, coordinate_hash>> dijkstra = dijkstra_predecessor_and_distance(
                         graph, waypoint);
                 policy_i.push_back(dijkstra.first);
                 distance_i.push_back(dijkstra.second);
                 targets_i.push_back(waypoint);
             }
         }
-        pair<unordered_map<Coordinate, vector<Coordinate>, coordinate_hash>, unordered_map<Coordinate, int, coordinate_hash>> dijkstra = dijkstra_predecessor_and_distance(
+        std::pair<std::unordered_map<Coordinate, std::vector<Coordinate>, coordinate_hash>, std::unordered_map<Coordinate, int, coordinate_hash>> dijkstra = dijkstra_predecessor_and_distance(
                 graph, v_F[i]);
         policy_i.push_back(dijkstra.first);
         distance_i.push_back(dijkstra.second);
@@ -58,16 +57,16 @@ Mstar::Mstar(vector<vector<int>> &grid, vector<pair<int, int>> &v_I_a, vector<ve
         distances.push_back(distance_i);
         targets.push_back(targets_i);
     }
-    Config_key v_I_key = Config_key(v_I, vector<int>(n_agents, 0));
+    Config_key v_I_key = Config_key(v_I, std::vector<int>(n_agents, 0));
     configurations[v_I_key] = Config_value();
     configurations[v_I_key].cost = 0;
     open.push(Queue_entry(heuristic_configuration(v_I_key), v_I_key));
 }
 
-pair<vector<vector<pair<int, int>>>, int> Mstar::solve() {
+std::pair<std::vector<std::vector<std::pair<int, int>>>, int> Mstar::solve() {
     while (!open.empty()) {
         Config_key v_k = open.top().config_key;
-        cout << v_k << endl;
+        std::cout << v_k << std::endl;
         Config_value v_k_config = configurations[v_k];
         open.pop();
         if (v_k.coordinates == v_F) {
@@ -80,29 +79,27 @@ pair<vector<vector<pair<int, int>>>, int> Mstar::solve() {
             }
             if (visited_waypoints) {
                 v_k_config.back_ptr.push_back(v_F);
-                vector<vector<pair<int, int>>> res;
+                std::vector<std::vector<std::pair<int, int>>> res;
                 for (int i = 0; i != n_agents; i++) {
-                    vector<pair<int, int>> res_i;
+                    std::vector<std::pair<int, int>> res_i;
                     for (auto &config : v_k_config.back_ptr) {
                         res_i.emplace_back(config[i].a, config[i].b);
                     }
                     res.push_back(res_i);
                 }
-                cout << "backprop1 took: " << backprop_time1
-                     << ". Which is hopefully the title of your sextape and was called: " << endl;
-                cout << "backprop2 took: " << backprop_time2
-                     << ". Which is hopefully the title of your sextape and was called: " << endl;
+                std::cout << "backprop1 took: " << backprop_time1 << std::endl;
+                std::cout << "backprop2 took: " << backprop_time2 << std::endl;
                 return make_pair(res, v_k_config.cost);
             }
         }
-        for (vector<Coordinate> &v_l : get_limited_neighbours(v_k)) {
-            vector<int> v_l_target_indices = v_k.targets;
+        for (std::vector<Coordinate> &v_l : get_limited_neighbours(v_k)) {
+            std::vector<int> v_l_target_indices = v_k.targets;
             for (int i = 0; i != n_agents; i++) {
                 if (v_l[i] == targets[i][v_l_target_indices[i]] && v_l[i] != v_F[i]) {
                     v_l_target_indices[i]++;
                 }
             }
-            set<int> v_l_collisions = phi(v_l, v_k.coordinates);
+            std::set<int> v_l_collisions = phi(v_l, v_k.coordinates);
             Config_key v_l_key = Config_key(v_l, v_l_target_indices);
             Config_value v_l_config;
             if (!configurations.count(v_l_key)) {
@@ -126,7 +123,7 @@ pair<vector<vector<pair<int, int>>>, int> Mstar::solve() {
             }
         }
     }
-    cout << "No path exists, or I am an idiot";
+    std::cout << "No path exists, or I am an idiot";
 }
 
 int Mstar::get_edge_weight(Config_key &v_k, Config_key &v_l) {
@@ -140,7 +137,7 @@ int Mstar::get_edge_weight(Config_key &v_k, Config_key &v_l) {
     return cost;
 }
 
-void Mstar::backprop(Config_key &v_k, unordered_set<int> &C_l) {
+void Mstar::backprop(Config_key &v_k, std::unordered_set<int> &C_l) {
     time.start();
     Config_value &current_config = configurations[v_k];
     bool subset = isSubset(C_l, current_config.collisions);
@@ -160,16 +157,16 @@ void Mstar::backprop(Config_key &v_k, unordered_set<int> &C_l) {
     backprop_time2 += float(time.elapsed());
 }
 
-vector<vector<Coordinate>> Mstar::get_limited_neighbours(Config_key &v_k) {
-    vector<vector<Coordinate>> options;
+std::vector<std::vector<Coordinate>> Mstar::get_limited_neighbours(Config_key &v_k) {
+    std::vector<std::vector<Coordinate>> options;
     for (int i = 0; i != n_agents; i++) {
         Coordinate source = v_k.coordinates[i];
-        vector<Coordinate> options_i;
+        std::vector<Coordinate> options_i;
         if (configurations[v_k].collisions.count(i)) {
             // Add all the neighbours
             options_i.push_back(source);
             int target_index = v_k.targets[i];
-            vector<Coordinate> successors = policies[i][target_index][source];
+            std::vector<Coordinate> successors = policies[i][target_index][source];
             if (successors.size() > 1) {
                 for (auto &successor : successors) {
                     options_i.push_back(successor);
@@ -181,7 +178,7 @@ vector<vector<Coordinate>> Mstar::get_limited_neighbours(Config_key &v_k) {
             }
         } else {
             int target_index = v_k.targets[i];
-            vector<Coordinate> successors = policies[i][target_index][source];
+            std::vector<Coordinate> successors = policies[i][target_index][source];
             if (successors.empty()) {
                 options_i.push_back(source);
             } else {
@@ -201,7 +198,7 @@ int Mstar::heuristic_configuration(Config_key &v_k) {
     int cost = 0;
     for (int i = 0; i != n_agents; i++) {
         int target_index = v_k.targets[i];
-        vector<Coordinate> targets_i = targets[i];
+        std::vector<Coordinate> targets_i = targets[i];
         cost += distances[i][target_index][v_k.coordinates[i]];
         target_index++;
         while (target_index < targets_i.size()) {
