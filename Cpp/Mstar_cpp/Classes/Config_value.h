e8b9421286099c201af408c8d3ba0d862b143ce0
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-05-22 13:29:42 +0200

    Made some changes to the CPP code inside the exe folder

--- Cpp/Mstar_cpp/Classes/Config_value.h
+++ Cpp/Mstar_cpp/Classes/Config_value.h
@@ -1,7 +1,7 @@
 class Config_value {
 public:
-	int cost = INT_MAX;
-	std::unordered_set<int> collisions{};
-	std::unordered_set<Config_key, config_key_hash> back_set{};
-	std::vector<std::vector<Coordinate>> back_ptr{};
+    int cost = INT_MAX;
+    std::unordered_set<int> collisions{};
+    std::unordered_set<Config_key, config_key_hash> back_set{};
+    std::vector<std::vector<Coordinate>> back_ptr{};
 };
