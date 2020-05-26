996ae07a7878d597874e837a8de400dc6a9a29ba
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-05-26 10:25:20 +0200

    Updated the main and added the pyd file

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import sys
+
+from Cpp.Mstar_pybind import Mstar
 from mapfw import MapfwBenchmarker
-
-from Python.mstar import Mstar
-from Python.test_python_benchmarks import setup_benchmark
-
 
 if __name__ == "__main__":
     benchmark = int(sys.argv[1])
     debug = sys.argv[2].lower() == 'true' if len(sys.argv) > 2 else True
     unordered = sys.argv[3].lower() == 'true' if len(sys.argv) > 3 else True
-    optimal = sys.argv[4].lower() == 'true' if len(sys.argv) > 4 else True
-    text = "Timon. Unordered: " + str(unordered) + ". Optimal: " + str(optimal)
+    text = "Timon. Unordered: " + str(unordered)
     benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", benchmark, "M*", text, debug)
     for problem in benchmarker:
-        graph, v_I, v_W, v_F = setup_benchmark(problem)
-        problem.add_solution(Mstar(graph, v_I, v_W, v_F, unordered, optimal).solve()[0])
+        cpp_waypoints = problem.waypoints
+        for i in range(len(cpp_waypoints)):
+            if len(cpp_waypoints[i]) == 0:
+                cpp_waypoints[i] = [[-1, -1]]
+        res = Mstar(problem.grid, problem.starts, cpp_waypoints, problem.goals, unordered).solve()
+        problem.add_solution(res[0])
--- mainCPP.py
+++ mainCPP.py
@@ -1,14 +1,11 @@
-from copy import copy
-
 from Cpp.Mstar_pybind import Mstar
 from mapfw import MapfwBenchmarker, get_all_benchmarks
 
-benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 33, "M*", "Cpp new cost function", False)
+benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 21, "M*", "Cpp tsp dynamic", False)
 for problem in benchmarker:
-    cpp_waypoints = copy(problem.waypoints)
+    cpp_waypoints = problem.waypoints
     for i in range(len(cpp_waypoints)):
         if len(cpp_waypoints[i]) == 0:
             cpp_waypoints[i] = [[-1, -1]]
-    res = Mstar(problem.grid, problem.starts, cpp_waypoints, problem.goals, False).solve()
-    print(res[1])
+    res = Mstar(problem.grid, problem.starts, cpp_waypoints, problem.goals, True).solve()
     problem.add_solution(res[0])
