3705ac31859ed84b77f2dde24043ad0db7e07931
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-05-28 16:43:56 +0200

    I still had it on inflated

--- mainPython.py
+++ mainPython.py
@@ -5,7 +5,7 @@ def solve(problem):
     from Python.test_python_benchmarks import setup_benchmark
     from Python.mstar import Mstar
     graph, v_I, v_W, v_F = setup_benchmark(problem)
-    return Mstar(graph, v_I, v_W, v_F, True).solve()[0]
+    return Mstar(graph, v_I, v_W, v_F).solve()[0]
 
 
 if __name__ == '__main__':
