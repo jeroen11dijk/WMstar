eca5b30902cf2d911295d7c10d6742a8165e711c
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-06-09 13:37:38 +0200

    New commit xD

--- mainPython.py
+++ mainPython.py
@@ -9,14 +9,14 @@ def solve(problem):
     return Mstar(graph, v_I, v_W, v_F, True).solve()[0]
 
 
-if __name__ == '__main__':
-    benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", [67, 68, 69], "M* Inflated", "Python", True, solver=solve,
-                                   cores=-1)
-    benchmarker.run()
+# if __name__ == '__main__':
+#     benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", [67, 68, 69], "M* Inflated", "Python", False, solver=solve,
+#                                    cores=-1)
+#     benchmarker.run()
 #
-# benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 5, "M*", "Python optimal?", True, solver=solve, cores=-1)
-# for problem in benchmarker:
-#     graph, v_I, v_W, v_F = setup_benchmark(problem)
-#     res = Mstar(graph, v_I, v_W, v_F, False).solve()[0]
-#     problem.add_solution(res)
-#     print(res)
+benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 17, "M*", "Python", True, solver=solve, cores=-1)
+for problem in benchmarker:
+    graph, v_I, v_W, v_F = setup_benchmark(problem)
+    res = Mstar(graph, v_I, v_W, v_F, False).solve()[0]
+    problem.add_solution(res)
+    print(res)
