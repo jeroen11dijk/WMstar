4b060324a948ab18feab093dd7bd4c23d6eaf074
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-06-12 12:05:32 +0200

    Setup for TU server

--- mainPython.py
+++ mainPython.py
@@ -1,6 +1,4 @@
 from mapfw import MapfwBenchmarker, get_all_benchmarks
-from Python.test_python_benchmarks import setup_benchmark
-from Python.mstar import Mstar
 
 def solve(problem):
     from Python.test_python_benchmarks import setup_benchmark
@@ -10,14 +8,7 @@ def solve(problem):
 
 
 if __name__ == '__main__':
-    benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", get_all_benchmarks(without=[10, 19, 21, 23, 24, 54, 58, 59, 61]), "M*", "Normal Python", False,
+    benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 1, "M*", "Python (TU)", True,
                                    solver=solve,
                                    cores=-1)
     benchmarker.run()
-
-# benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 1, "M*", "Normal Python", True, solver=solve, cores=-1)
-# for problem in benchmarker:
-#     graph, v_I, v_W, v_F = setup_benchmark(problem)
-#     res = Mstar(graph, v_I, v_W, v_F, False).solve()[0]
-#     problem.add_solution(res)
-#     print(res)
