139dbb6f9700487ef7e159efe2e443ee3f59c96d
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-06-15 20:32:57 +0200

    Redo of 75

--- mainPython.py
+++ mainPython.py
@@ -5,12 +5,11 @@ def solve(problem):
     from Python.test_python_benchmarks import setup_benchmark
     from Python.mstar import Mstar
     graph, v_I, v_W, v_F = setup_benchmark(problem)
-    res = Mstar(graph, v_I, v_W, v_F, False).solve()[0]
+    res = Mstar(graph, v_I, v_W, v_F, True).solve()[0]
     return res
 
 
 if __name__ == '__main__':
-    benchmarks = [85, 86, 87, 88, 89 ,90, 91]
-    benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", benchmarks, "M*", "Python (TU)", False,
+    benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 75, "M* Inflated", "Python (TU)", False,
                                    solver=solve, cores=10)
     benchmarker.run()
