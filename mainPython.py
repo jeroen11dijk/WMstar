7ae2827ff3c9683b320d71c1242e7bd48017ef60
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-06-15 08:34:33 +0200

    Setup for all the progressive benchmarks

--- mainPython.py
+++ mainPython.py
@@ -5,11 +5,12 @@ def solve(problem):
     from Python.test_python_benchmarks import setup_benchmark
     from Python.mstar import Mstar
     graph, v_I, v_W, v_F = setup_benchmark(problem)
-    res = Mstar(graph, v_I, v_W, v_F, True).solve()[0]
+    res = Mstar(graph, v_I, v_W, v_F, False).solve()[0]
     return res
 
 
 if __name__ == '__main__':
-    benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", [31, 32, *range(34, 52)] * 10, "M* Inflated", "Python (TU)", False,
-                                   solver=solve, cores=3, timeout=20000)
+    benchmarks = [67, 68, 69, 70, 71, 72, 73, 74, 75, 84, 85, 86, 87, 88, 89, 90, 91, 79, 80, 81, 82]
+    benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", benchmarks, "M*", "Python (TU)", False,
+                                   solver=solve, cores=3)
     benchmarker.run()
