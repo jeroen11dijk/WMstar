7275999fd8cb067bc4883d3a5c28714568d6b0e8
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-06-15 19:57:16 +0200

    Normal version for DAO

--- mainPython.py
+++ mainPython.py
@@ -5,12 +5,12 @@ def solve(problem):
     from Python.test_python_benchmarks import setup_benchmark
     from Python.mstar import Mstar
     graph, v_I, v_W, v_F = setup_benchmark(problem)
-    res = Mstar(graph, v_I, v_W, v_F, True).solve()[0]
+    res = Mstar(graph, v_I, v_W, v_F, False).solve()[0]
     return res
 
 
 if __name__ == '__main__':
-    benchmarks = [67, 68, 69, 70, 71, 72, 73, 74, 75]
-    benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", benchmarks, "M* Inflated", "Python (TU)", False,
+    benchmarks = [85, 86, 87, 88, 89 ,90, 91]
+    benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", benchmarks, "M*", "Python (TU)", False,
                                    solver=solve, cores=10)
     benchmarker.run()
