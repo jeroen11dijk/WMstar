2e6e8a3b028b49cc0d1c3af85f5c0594763a6f29
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-06-15 13:30:38 +0200

    Timon's progressive benchmarks Inflated

--- mainPython.py
+++ mainPython.py
@@ -5,12 +5,12 @@ def solve(problem):
     from Python.test_python_benchmarks import setup_benchmark
     from Python.mstar import Mstar
     graph, v_I, v_W, v_F = setup_benchmark(problem)
-    res = Mstar(graph, v_I, v_W, v_F, False).solve()[0]
+    res = Mstar(graph, v_I, v_W, v_F, True).solve()[0]
     return res
 
 
 if __name__ == '__main__':
     benchmarks = [79, 80, 81, 82]
-    benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", benchmarks, "M*", "Python (TU)", False,
+    benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", benchmarks, "M* Inflated", "Python (TU)", False,
                                    solver=solve, cores=10)
     benchmarker.run()
