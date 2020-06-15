37d0e3f1049659d913657ce72866712f3a385fdb
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-06-15 11:57:55 +0200

    Unlimited cores

--- mainPython.py
+++ mainPython.py
@@ -10,7 +10,7 @@ def solve(problem):
 
 
 if __name__ == '__main__':
-    benchmarks = [67, 68, 69, 70, 71, 72, 73, 74, 75, 84, 85, 86, 87, 88, 89, 90, 91, 79, 80, 81, 82]
+    benchmarks = [74, 75, 84, 85, 86, 87, 88, 89, 90, 91, 79, 80, 81, 82]
     benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", benchmarks, "M*", "Python (TU)", False,
-                                   solver=solve, cores=3)
+                                   solver=solve, cores=10)
     benchmarker.run()
