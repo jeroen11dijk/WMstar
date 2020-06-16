a2e904261f9b05566f08dfe38e764253307ab8b9
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-06-16 13:32:48 +0200

    Updated the graphs and added them to the repo

--- graphs/Graph_normal.py
+++ graphs/Graph_normal.py
@@ -4,7 +4,7 @@ import csv
 x_16165 = []
 y_16165 = []
 
-with open('16165normal.csv', 'r') as csvfile:
+with open('CSV/16165normal.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x_16165.append(int(row[0]))
@@ -13,7 +13,7 @@ with open('16165normal.csv', 'r') as csvfile:
 x_161610 = []
 y_161610 = []
 
-with open('161610normal.csv', 'r') as csvfile:
+with open('CSV/161610normal.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x_161610.append(int(row[0]))
@@ -22,7 +22,7 @@ with open('161610normal.csv', 'r') as csvfile:
 x_161613 = []
 y_161613 = []
 
-with open('161613normal.csv', 'r') as csvfile:
+with open('CSV/161613normal.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x_161613.append(int(row[0]))
@@ -31,7 +31,7 @@ with open('161613normal.csv', 'r') as csvfile:
 x_32325 = []
 y_32325 = []
 
-with open('32325normal.csv', 'r') as csvfile:
+with open('CSV/32325normal.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x_32325.append(int(row[0]))
@@ -40,7 +40,7 @@ with open('32325normal.csv', 'r') as csvfile:
 x_323210 = []
 y_323210 = []
 
-with open('323210normal.csv', 'r') as csvfile:
+with open('CSV/323210normal.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x_323210.append(int(row[0]))
@@ -49,7 +49,7 @@ with open('323210normal.csv', 'r') as csvfile:
 x_323213 = []
 y_323213 = []
 
-with open('323213normal.csv', 'r') as csvfile:
+with open('CSV/323213normal.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x_323213.append(int(row[0]))
@@ -58,7 +58,7 @@ with open('323213normal.csv', 'r') as csvfile:
 x_64645 = []
 y_64645 = []
 
-with open('64645normal.csv', 'r') as csvfile:
+with open('CSV/64645normal.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x_64645.append(int(row[0]))
@@ -67,7 +67,7 @@ with open('64645normal.csv', 'r') as csvfile:
 x_646410 = []
 y_646410 = []
 
-with open('646410normal.csv', 'r') as csvfile:
+with open('CSV/646410normal.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x_646410.append(int(row[0]))
@@ -76,23 +76,24 @@ with open('646410normal.csv', 'r') as csvfile:
 x_646413 = []
 y_646413 = []
 
-with open('646413normal.csv', 'r') as csvfile:
+with open('CSV/646413normal.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x_646413.append(int(row[0]))
         y_646413.append(int(row[1]))
 
-plt.plot(x_16165, y_16165, label='16x16, 5')
-plt.plot(x_161610, y_161610, label='16x16, 10')
-plt.plot(x_161613, y_161613, label='16x16, 13')
-plt.plot(x_32325, y_32325, label='32x32, 5')
-plt.plot(x_323210, y_323210, label='32x32, 10')
-plt.plot(x_323213, y_323213, label='32x32, 13')
-plt.plot(x_64645, y_64645, label='64x64, 5')
-plt.plot(x_646410, y_646410, label='64x64, 10')
-plt.plot(x_646413, y_646413, label='64x64, 13')
-plt.xlabel('Agents')
-plt.ylabel('Succeeded')
-plt.title('M*')
+plt.plot(x_16165, y_16165, label='Mapsize of 16x16, with 5 waypoints')
+plt.plot(x_161610, y_161610, label='Mapsize of 16x16, with 10 waypoints')
+plt.plot(x_161613, y_161613, label='Mapsize of 16x16, with 13 waypoints')
+plt.plot(x_32325, y_32325, label='Mapsize of 32x32, with 5 waypoints')
+plt.plot(x_323210, y_323210, label='Mapsize of 32x32, with 10 waypoints')
+plt.plot(x_323213, y_323213, label='Mapsize of 32x32, with 13 waypoints')
+plt.plot(x_64645, y_64645, label='Mapsize of 64x64, with 5 waypoints')
+plt.plot(x_646410, y_646410, label='Mapsize of 64x64, with 10 waypoints')
+plt.plot(x_646413, y_646413, label='Mapsize of 64x64, with 13 waypoints')
+plt.xlabel('Number of agents')
+plt.ylabel('Successful runs')
+plt.title('WM*')
 plt.legend()
+plt.ylim(bottom=0)
 plt.show()
