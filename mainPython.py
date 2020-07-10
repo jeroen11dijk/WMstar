9a85e2b3b4fd70664ff20bda9f6b440c306a5e51
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-07-10 15:24:20 +0200

    Setup ordered run for server

--- mainPython.py
+++ mainPython.py
@@ -1,4 +1,4 @@
-from mapfw import MapfwBenchmarker
+from mapfw import MapfwBenchmarker, get_all_benchmarks
 
 
 def solve(problem):
@@ -10,6 +10,6 @@ def solve(problem):
 
 
 if __name__ == '__main__':
-    benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", 1, "M* Inflated", "Python Ordered", True,
+    benchmarker = MapfwBenchmarker("42cf6ce8D2A5B954", get_all_benchmarks(without=[10, 19, 21, 23, 24, 54, 58, 59, 61]), "WM*", "Python Ordered", False,
                                    solver=solve, cores=1)
     benchmarker.run()
